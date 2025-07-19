ba7319386858653f988617fec5c414b1e465f6e4
tontacchi <andy.tonthat14@gmail.com>
2025-07-18 22:39:19 -0700

    chore(nvim): disable modeline for markdown files using autocmd

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,13 @@ require("autocmds")           -- Load auto commands
 require("config.lazy")        -- looks for ~/.config/nvim/lua/config/lazy.lua
 require("config.appearance")
 
+
+-- ignore - ex: something in .md files please
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+	vim.opt_local.modeline = false
+  end,
+  group = vim.api.nvim_create_augroup("MarkdownNoModeline", { clear = true }),
+})
+
