e27b1138a5df256f23a63e8303c7952a99ae6d25
tontacchi <andy.tonthat14@gmail.com>
2025-07-18 22:36:14 -0700

    feat(aliases): organize and expand with section headers and new directory shortcuts

--- aliases_bash.sh
+++ aliases_bash.sh
@@ -1,8 +1,25 @@
-# lmao
+#―――[ Common Directories ]――――――――――――――――――――――――――――――――――――――――――――――――――――――
+alias res="cd $HOME/pfiles/workspace/local/res"
+alias monk="cd $HOME/pfiles/workspace/github.com/tontacchi/monkey"
+alias crawl="cd $HOME/pfiles/workspace/github.com/tontacchi/crawler"
+
+alias github="cd $HOME/pfiles/workspace/github.com"
+alias theory="cd $HOME/pfiles/books/textbooks/CS-Theory/"
+
+alias i3conf="cd $HOME/.config/i3"
+alias picomconf="cd $HOME/.config/picom"
+
+#―――[ Common Directories ]――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+
+#―――[ Silly ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# lmao thanks reddit
 alias please="sudo"
 alias fucking="sudo"
 
-# git shortcuts
+#―――[ Silly ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+#―――[ git Shortcuts ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 alias gd="git diff"
 
 alias gs="git status"
@@ -19,12 +36,17 @@ alias gb="git branch"
 alias gi="git init"
 alias gcl="git clone"
 
-# quick ls variations
+#―――[ git Shortcuts ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+
+#―――[ ls Variations ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 alias lf="ls -F";
 alias ll="ls -lh";
 alias lw="ls -w 70";
 alias lwf="ls -w 70 -F";
 alias lwaf="lw -aFw 70";
+
+#―――[ ls Variations ]―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
 alias ..="cd ..";
 alias fhere="find . -name";
@@ -32,26 +54,11 @@ alias fhere="find . -name";
 alias :q="exit"
 
 # sector 0's local ip
-alias knockknock="ping 192.168.1.226";
-
-# Trick 1: list, sorted by file size
-alias lfs="ls --human-readable --size -1 -S --classify";
-
-# Trick 2: Find command in bash history
-# alias gh="history | grep"
-
+alias localhost="ping 192.168.1.226";
 alias myip="curl http://ipecho.net/plain; echo";
-
-# get the top 10 commands used from history
-# history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
 
 # ssh into username@openlab.ics.uci.edu
 alias ics="ssh aatontha@circinus-60.ics.uci.edu"
-
-# print newline after any command executes
-# PROMPT_COMMAND="export PROMPT_COMMAND=echo";
-# alias clear="unset PROMPT_COMMAND; clear; PROMPT_COMMAND='export PROMPT_COMMAND=echo';";
-# alias clear_print="unset PROMPT_COMMAND; clear; PROMPT_COMMAND='export PROMPT_COMMAND=echo';";
 
 # try: keep terminal color scheme for tmux
 alias tmux="TERM=screen-256color-bce tmux"
