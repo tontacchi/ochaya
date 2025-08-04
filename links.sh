a638a7d526a09796d4adf25e2b2cbacc28c4caa3
tontacchi <andy.tonthat14@gmail.com>
2025-08-04 06:56:57 -0700

    chore: moved files -> bash/ & updated symlinks

--- links.sh
+++ links.sh
@@ -2,11 +2,13 @@
 
 DOTDIR="$HOME/.ochaya"
 
-ln -sf "${DOTDIR}/bashrc"            "$HOME/.bashrc"
-ln -sf "${DOTDIR}/aliases_bash.sh"   "$HOME/.bash_aliases"
-ln -sf "${DOTDIR}/functions_bash.sh" "$HOME/.bash_functions"
+ln -sf "${DOTDIR}/bash/bashrc"       "$HOME/.bashrc"
+ln -sf "${DOTDIR}/bash/aliases.sh"   "$HOME/.bash_aliases"
+ln -sf "${DOTDIR}/bash/functions.sh" "$HOME/.bash_functions"
+
+ln -sf "${DOTDIR}/nvim"              "$HOME/.config/nvim"
+
 ln -sf "${DOTDIR}/vimrc"             "$HOME/.vimrc"
 ln -sf "${DOTDIR}/tmux.conf"         "$HOME/.tmux.conf"
-ln -sf "${DOTDIR}/nvim"              "$HOME/.config/nvim"
 
 echo "Symlinks updated to point to ~/${DOTDIR}"
