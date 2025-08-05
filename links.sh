90a4f616e30bd6f1d50359f2e38f07abc3c97fcb
tontacchi <andy.tonthat14@gmail.com>
2025-08-05 10:37:17 -0700

    feat: added safe_link() to remove existing symlinks -> avoid circular link setting

--- links.sh
+++ links.sh
@@ -3,21 +3,29 @@
 dots="$HOME/.ochaya"
 conf="$HOME/.config"
 
-ln -sf "${dots}/bash/bashrc"       "$HOME/.bashrc"
-ln -sf "${dots}/bash/aliases.sh"   "$HOME/.bash_aliases"
-ln -sf "${dots}/bash/functions.sh" "$HOME/.bash_functions"
+safe_link() {
+	src="$1"
+	dest="$2"
 
-ln -sf "${dots}/nvim"      "${conf}/nvim"
-ln -sf "${dots}/vimrc"     "$HOME/.vimrc"
-ln -sf "${dots}/tmux.conf" "$HOME/.tmux.conf"
+	if [[ -e "$dest" ]] || [[ -L "$dest" ]]; then
+		rm -rf "$dest"
+	fi
+	
+	ln -s "$src" "$dest"
+}
 
-ln -sf "${dots}/i3"      "${conf}/i3"
-ln -sf "${dots}/picom"   "${conf}/picom"
-ln -sf "${dots}/polybar" "${conf}/polybar"
-ln -sf "${dots}/rofi"    "${conf}/rofi"
-ln -sf "${dots}/dunst"   "${conf}/dunst"
-
-ln -sf "${dots}/neofetch/"              "${conf}/neofetch"
-ln -sf "${dots}/starship/starship.toml" "${conf}/starship.toml"
+# safe_link "${dots}/bash/bashrc"       "$HOME/.bashrc"
+# safe_link "${dots}/bash/aliases.sh"   "$HOME/.bash_aliases"
+# safe_link "${dots}/bash/functions.sh" "$HOME/.bash_functions"
+# safe_link "${dots}/nvim"      "${conf}/nvim"
+# safe_link "${dots}/vimrc"     "$HOME/.vimrc"
+# safe_link "${dots}/tmux.conf" "$HOME/.tmux.conf"
+# safe_link "${dots}/i3"      "${conf}/i3"
+# safe_link "${dots}/picom"   "${conf}/picom"
+# safe_link "${dots}/polybar" "${conf}/polybar"
+# safe_link "${dots}/rofi"    "${conf}/rofi"
+safe_link "${dots}/dunst"   "${conf}/dunst"
+safe_link "${dots}/neofetch/"              "${conf}/neofetch"
+safe_link "${dots}/starship/starship.toml" "${conf}/starship.toml"
 
 echo "Symlinks updated to point to ~/${dots}"
