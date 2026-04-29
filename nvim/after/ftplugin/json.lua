386d7ead52e308c8742b1db8bc13f94011a50e4b
tontacchi <andy.tonthat14@gmail.com>
2026-04-29 04:47:02 -0700

    nvim: removed lua shiftwidth 4, appearance uses termguicolors = true

---
+++ nvim/after/ftplugin/json.lua
@@ -0,0 +1,7 @@
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true -- Use spaces instead of tab characters
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+
--- nvim/lua/config/appearance.lua
+++ nvim/lua/config/appearance.lua
@@ -1,3 +1,5 @@
+vim.opt.termguicolors = true
+
 -- Make the background transparent
 vim.api.nvim_set_hl(0, "Normal",       { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat",  { bg = "none" })
